e12183ec33e5912964ddeac238fdaa8c5e926845
Jess Langford <dvm22syu@uea.ac.uk>
2025-01-28 11:49:28 +0000

    My first commit into my first repo

---
+++ chapter 3 code.R
@@ -0,0 +1,7 @@
+library(usethis) 
+install.packages('usethis')
+use_git_config(user.name  = "Jess Langford",
+               user.email = "dvm22syu@uea.ac.uk")
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+usethis::gh_token_help()
