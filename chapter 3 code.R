610caefc1ec9195fa95283dd8a3cb65dd7f7243b
Jess Langford <dvm22syu@uea.ac.uk>
2025-01-28 12:06:53 +0000

    packages

--- chapter 3 code.R
+++ chapter 3 code.R
@@ -1,7 +1,12 @@
-library(usethis) 
-install.packages('usethis')
-use_git_config(user.name  = "Jess Langford",
-               user.email = "dvm22syu@uea.ac.uk")
-usethis::create_github_token()
-gitcreds::gitcreds_set()
-usethis::gh_token_help()
+# library(usethis) 
+# install.packages('usethis')
+# use_git_config(user.name  = "Jess Langford",
+#                user.email = "dvm22syu@uea.ac.uk")
+# usethis::create_github_token()
+# gitcreds::gitcreds_set()
+# usethis::gh_token_help()
+
+library(tidyverse)
+library(palmerpenguins)
+install.packages('tidyverse')
+install.packages('palmerpenguins')
